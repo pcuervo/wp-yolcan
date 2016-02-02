67868d6a41506a24db83b0f3814d0aef65bafda0
alejandro <alex@losmaquiladores.com>
2016-02-02 13:39:50 -0600

    cambios

--- themes/yolcan/js/functions.js
+++ themes/yolcan/js/functions.js
@@ -519,7 +519,7 @@
 
 		    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
 		    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-		        this.setZoom(14);
+		        this.setZoom(10);
 		        google.maps.event.removeListener(boundsListener);
 		    });
 		    
