72532c36852d26891cfdfb635eebd355b0e5dae7
Nayeli Jordan <raul@zamacona.me>
2016-02-12 09:48:48 -0600

    main con container

--- plugins/woocommerce/templates/single-product/tabs/tabs.php
+++ plugins/woocommerce/templates/single-product/tabs/tabs.php
@@ -34,10 +34,10 @@ if ( ! empty( $tabs ) ) : ?>
 		</div>
 	</div>
 	<div class="woocommerce-tabs wc-tabs-wrapper">
-		<ul class="tabs wc-tabs">
+		<ul class="tabs wc-tabs [ list-style-none ]">
 			<?php foreach ( $tabs as $key => $tab ) : ?>
 				<li class="<?php echo esc_attr( $key ); ?>_tab">
-					<a href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
+					<a class="[ color-secondary ]" href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
 				</li>
 			<?php endforeach; ?>
 		</ul>
