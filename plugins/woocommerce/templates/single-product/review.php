ce845281b5629cfd3d99c0955c3f618ac301d03b
Nayeli Jordan <raul@zamacona.me>
2016-02-12 11:26:15 -0600

    carpeta woocommerce

--- plugins/woocommerce/templates/single-product/review.php
+++ plugins/woocommerce/templates/single-product/review.php
@@ -27,45 +27,48 @@ $verified = wc_review_is_from_verified_owner( $comment->comment_ID );
 ?>
 <li itemprop="review" itemscope itemtype="http://schema.org/Review" <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
 
-	<div id="comment-<?php comment_ID(); ?>" class="comment_container">
+	<div id="comment-<?php comment_ID(); ?>" class="comment_container [ row ]">
 
-		<?php echo get_avatar( $comment, apply_filters( 'woocommerce_review_gravatar_size', '60' ), '' ); ?>
-
-		<div class="comment-text">
-
-			<?php if ( $rating && get_option( 'woocommerce_enable_review_rating' ) === 'yes' ) : ?>
-
-				<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating" class="star-rating" title="<?php echo sprintf( __( 'Rated %d out of 5', 'woocommerce' ), $rating ) ?>">
-					<span style="width:<?php echo ( $rating / 5 ) * 100; ?>%"><strong itemprop="ratingValue"><?php echo $rating; ?></strong> <?php _e( 'out of 5', 'woocommerce' ); ?></span>
+				<div class="[ col-xs-12 col-sm-offset-3 col-sm-2 ]">
+					<?php echo get_avatar( $comment, apply_filters( 'woocommerce_review_gravatar_size', '60' ), '' ); ?>
 				</div>
 
-			<?php endif; ?>
+				<div class="comment-text [ col-xs-12 col-sm-4 ]">
 
-			<?php do_action( 'woocommerce_review_before_comment_meta', $comment ); ?>
+					<?php if ( $rating && get_option( 'woocommerce_enable_review_rating' ) === 'yes' ) : ?>
 
-			<?php if ( $comment->comment_approved == '0' ) : ?>
+						<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating" class="star-rating" title="<?php echo sprintf( __( 'Rated %d out of 5', 'woocommerce' ), $rating ) ?>">
+							<span style="width:<?php echo ( $rating / 5 ) * 100; ?>%"><strong itemprop="ratingValue"><?php echo $rating; ?></strong> <?php _e( 'out of 5', 'woocommerce' ); ?></span>
+						</div>
 
-				<p class="meta"><em><?php _e( 'Your comment is awaiting approval', 'woocommerce' ); ?></em></p>
+					<?php endif; ?>
 
-			<?php else : ?>
+					<?php do_action( 'woocommerce_review_before_comment_meta', $comment ); ?>
 
-				<p class="meta">
-					<strong itemprop="author"><?php comment_author(); ?></strong> <?php
+					<?php if ( $comment->comment_approved == '0' ) : ?>
 
-						if ( get_option( 'woocommerce_review_rating_verification_label' ) === 'yes' )
-							if ( $verified )
-								echo '<em class="verified">(' . __( 'verified owner', 'woocommerce' ) . ')</em> ';
+						<p class="meta"><em><?php _e( 'Your comment is awaiting approval', 'woocommerce' ); ?></em></p>
 
-					?>&ndash; <time itemprop="datePublished" datetime="<?php echo get_comment_date( 'c' ); ?>"><?php echo get_comment_date( wc_date_format() ); ?></time>:
-				</p>
+					<?php else : ?>
 
-			<?php endif; ?>
+						<p class="meta">
+							<strong itemprop="author"><?php comment_author(); ?></strong> <?php
 
-			<?php do_action( 'woocommerce_review_before_comment_text', $comment ); ?>
+								if ( get_option( 'woocommerce_review_rating_verification_label' ) === 'yes' )
+									if ( $verified )
+										echo '<em class="verified">(' . __( 'verified owner', 'woocommerce' ) . ')</em> ';
 
-			<div itemprop="description" class="description"><?php comment_text(); ?></div>
+							?>&ndash; <time itemprop="datePublished" datetime="<?php echo get_comment_date( 'c' ); ?>"><?php echo get_comment_date( wc_date_format() ); ?></time>:
+						</p>
 
-			<?php do_action( 'woocommerce_review_after_comment_text', $comment ); ?>
+					<?php endif; ?>
 
-		</div>
+					<?php do_action( 'woocommerce_review_before_comment_text', $comment ); ?>
+
+					<div itemprop="description" class="description"><?php comment_text(); ?></div>
+
+					<?php do_action( 'woocommerce_review_after_comment_text', $comment ); ?>
+
+				</div>
+
 	</div>
--- themes/yolcan/woocommerce/tabs.php
+++ themes/yolcan/woocommerce/tabs.php
@@ -29,19 +29,23 @@ $tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
 if ( ! empty( $tabs ) ) : ?>
 
-	<div class="woocommerce-tabs wc-tabs-wrapper">
-		<ul class="tabs wc-tabs">
-			<?php foreach ( $tabs as $key => $tab ) : ?>
-				<li class="<?php echo esc_attr( $key ); ?>_tab">
-					<a href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-		<?php foreach ( $tabs as $key => $tab ) : ?>
-			<div class="panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
-				<?php call_user_func( $tab['callback'], $key, $tab ); ?>
+		<div class="[ col-xs-12 col-sm-6 ]">
+			<div class="woocommerce-tabs wc-tabs-wrapper">
+				<ul class="tabs wc-tabs [ list-style-none no-padding ]">
+					<?php foreach ( $tabs as $key => $tab ) : ?>
+						<li class="<?php echo esc_attr( $key ); ?>_tab [ info ]">
+							<a class="" href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+				<?php foreach ( $tabs as $key => $tab ) : ?>
+					<div class="panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
+						<?php call_user_func( $tab['callback'], $key, $tab ); ?>
+					</div>
+				<?php endforeach; ?>
 			</div>
-		<?php endforeach; ?>
+		</div>
 	</div>
+
 
 <?php endif; ?>
