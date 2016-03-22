eec564dec05eaecf80e5c6d24df0b3b0685aa580
alejandro <alex@losmaquiladores.com>
2016-03-22 16:40:28 -0600

    actualizacion de plugin

--- plugins/woocommerce/templates/content-single-product.php
+++ plugins/woocommerce/templates/content-single-product.php
@@ -47,25 +47,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 		do_action( 'woocommerce_before_single_product_summary' );
 	?>
 
-		<div class="summary entry-summary">
+	<div class="summary entry-summary">
 
-			<?php
-				/**
-				 * woocommerce_single_product_summary hook.
-				 *
-				 * @hooked woocommerce_template_single_title - 5
-				 * @hooked woocommerce_template_single_rating - 10
-				 * @hooked woocommerce_template_single_price - 10
-				 * @hooked woocommerce_template_single_excerpt - 20
-				 * @hooked woocommerce_template_single_add_to_cart - 30
-				 * @hooked woocommerce_template_single_meta - 40
-				 * @hooked woocommerce_template_single_sharing - 50
-				 */
-				do_action( 'woocommerce_single_product_summary' );
-			?>
+		<?php
+			/**
+			 * woocommerce_single_product_summary hook.
+			 *
+			 * @hooked woocommerce_template_single_title - 5
+			 * @hooked woocommerce_template_single_rating - 10
+			 * @hooked woocommerce_template_single_price - 10
+			 * @hooked woocommerce_template_single_excerpt - 20
+			 * @hooked woocommerce_template_single_add_to_cart - 30
+			 * @hooked woocommerce_template_single_meta - 40
+			 * @hooked woocommerce_template_single_sharing - 50
+			 */
+			do_action( 'woocommerce_single_product_summary' );
+		?>
 
-		</div><!-- .summary -->
-
+	</div><!-- .summary -->
 
 	<?php
 		/**
