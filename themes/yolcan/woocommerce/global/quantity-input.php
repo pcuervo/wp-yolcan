754224023785ffeae3373573b226a7455dde300f
Nayeli-Jordn <nayeli.jordan16@gmail.com>
2016-05-18 19:02:56 -0500

    link a recetas desde mi cuenta

--- themes/yolcan/woocommerce/global/quantity-input.php
+++ themes/yolcan/woocommerce/global/quantity-input.php
@@ -19,6 +19,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<div class="quantity [ inline-block ][ align-middle ]">
+<div class="quantity [ inline-block ][ align-middle ][ margin-bottom ]">
 	<input type="number" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( $max_value ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="input-text qty text [ width-80 padding--xsmall ][ form-control inline-block no-border no-border-radius bg-gray ]" size="4" />
 </div>
