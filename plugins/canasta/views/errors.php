5a7f942189d59aef92745f5cef2666759f708a59
Raúl De Zamacona <raulppcuervo.com>
2016-05-19 16:03:41 -0500

    plugins

--- plugins/canasta/views/errors.php
+++
@@ -1,8 +0,0 @@
-<div class="error">
-	<div class="header">Tiene los siguiente errores</div>
-	<?php if (!empty($mesage_error)) : 
-		foreach ($mesage_error as $key => $mensaje): ?>
-			<div class="mensaje-error"><?php echo $mensaje; ?></div>
-		<?php endforeach; 
-	endif; ?>
-</div>
