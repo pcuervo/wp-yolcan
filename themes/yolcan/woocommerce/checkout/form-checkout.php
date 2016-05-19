ed931536bc35d0808fb5ca51beed4487829eb639
Nayeli-Jordn <nayeli.jordan16@gmail.com>
2016-05-18 19:27:07 -0500

    col micuenta eliminado

--- themes/yolcan/woocommerce/checkout/form-checkout.php
+++ themes/yolcan/woocommerce/checkout/form-checkout.php
@@ -39,7 +39,7 @@ if ( ! $checkout->enable_signup && ! $checkout->enable_guest_checkout && ! is_us
 
 			<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-			<div class="col2-set" id="customer_details">
+			<div class="" id="customer_details">
 				<div class="[ row ]">
 					<?php do_action( 'woocommerce_checkout_billing' ); ?>
 				</div>
