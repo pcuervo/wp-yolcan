3374a6c08ed3b34bc50f087318321f28439954ac
alejandro <alejandro_mennn@hotmail.com>
2016-07-19 17:32:36 -0500

    parte 2 mi canasta

--- themes/yolcan/woocommerce/myaccount/templates/canastas-cliente.php
+++ themes/yolcan/woocommerce/myaccount/templates/canastas-cliente.php
@@ -2,6 +2,7 @@
 global $opCliente;
 global $clubCanasta;
 $costoSemanal = isset($clubCanasta->attr_variation->costoSemanal) ? $clubCanasta->attr_variation->costoSemanal : 0; ?>
+<span id="productos-canasta"></span>
 <article class="[ padding--bottom border-bottom margin-bottom ]">
 	<h4>Tu próxima canasta - <span class="[ color-primary ]">$<?php echo $costoSemanal;?></span></h4>
 	<p><?php echo $clubCanasta->producto_name; ?> para el 10 de junio:</p>
@@ -42,81 +43,53 @@ $costoSemanal = isset($clubCanasta->attr_variation->costoSemanal) ? $clubCanasta
 
 <article class="">
 	<h4>Agrega productos</h4>
-	<p>Selecciona los productos que deseas agregar a tu canasta:</p>
-		<?php if (! empty($canasta) AND method_exists($canasta, 'getIngredientesAdicionales')) :
-			$adicionales = $canasta->getIngredientesAdicionales();
-			if ( ! empty($adicionales) ):
-				foreach($adicionales as $data_ingrediente):
-					$ingrediente = get_post($data_ingrediente->ingrediente_id);
-                    $precio = get_post_meta($data_ingrediente->ingrediente_id, 'precio_ingrediente', true);
-                    $peso = get_post_meta($data_ingrediente->ingrediente_id, 'cantidad_en_peso', true);
-                    $tipo = get_post_meta($data_ingrediente->ingrediente_id, 'tipo_en_peso', true);
-                    $user_id = get_current_user_id();
-                    $user = get_userdata( $user_id );
-                    if(isset($_POST['padicional'])) {
-                            if($_POST['padicionalnumber']=='')
-                            {
-                                echo "Por favor indique el número.";
-                            }
-                            else
-                            {
-                                $postid = $current_user->ID;
-                                $padicionalnombre = $_POST['padicionalnombre'];
-                                $padicionalprecio = $_POST['padicionalprecio'];
-                                $padicionalpeso = $_POST['padicionalpeso'];
-                                $padicionaltipo = $_POST['padicionaltipo'];
-                                $padicionalnumber = $_POST['padicionalnumber'];
-                                $padicionalocasion = $_POST['padicionalocasion'];
-                                update_post_meta($postid, 'padicionalnombre', $padicionalnombre );
-                                update_post_meta($postid, 'padicionalprecio', $padicionalprecio );
-                                update_post_meta($postid, 'padicionalpeso', $padicionalpeso );
-                                update_post_meta($postid, 'padicionaltipo', $padicionaltipo );
-                                update_post_meta($postid, 'padicionalnumber', $padicionalnumber );
-                                update_post_meta($postid, 'padicionalocasion', $padicionalocasion );
-                            }
-                    }
-                    $padicionalnombre = get_post_meta($current_user->ID, 'padicionalnombre', true);
-                    $padicionalprecio = get_post_meta($current_user->ID, 'padicionalprecio', true);
-                    $padicionalpeso = get_post_meta($current_user->ID, 'padicionalpeso', true);
-                    $padicionaltipo = get_post_meta($current_user->ID, 'padicionaltipo', true);
-                    $padicionalnumber = get_post_meta($current_user->ID, 'padicionalnumber', true);
-                    $padicionalocasion = get_post_meta($current_user->ID, 'padicionalocasion', true);
-                    ?>
-                    <form method="post" id="contactus_form" action="">
-					<div class="[ margin-bottom ]">
-						<a data-toggle="collapse" href="#<?php echo $ingrediente->post_name; ?>" class="[ no-decoration color-dark color-dark-hover ]">
-							<button type="submit" class="[ inline-block align-middle ][ btn btn-secondary ]">+</button>
-							<p class="[ inline-block align-middle ][ no-margin ]"><?php echo $ingrediente->post_title; ?></p>
-                            <input type="hidden" name="padicionalnombre" value="<?php echo $ingrediente->post_title; ?>">
-						</a>
-						<div id="<?php echo $ingrediente->post_name; ?>" class="[ panel-collapse collapse ][ padding--top ]">
-							<p class="[ color-gray-xlight ]">Precio: $ <?php echo number_format($precio); ?>
-                                <input type="hidden" name="padicionalprecio" value="<?php echo number_format($precio); ?>">
-                            <br>Peso: <?php echo $peso; ?> <?php echo $tipo; ?></p>
-                                <input type="hidden" name="padicionalpeso" value="<?php echo $peso; ?>">
-                                <input type="hidden" name="padicionaltipo" value="<?php echo $tipo; ?>">
-							<div class="[ row ]">
-								<div class="[ col-xs-3 padding--right--small ]">
-									<input type="number" name="padicionalnumber" class="[ width-90 padding--xsmall ][ form-control no-border no-border-radius bg-gray ]">
+	<?php if (!empty($clubCanasta->adicionales) AND $costoSemanal < $opCliente->saldo): ?>
+		
+		<p>Selecciona los productos que deseas agregar a tu canasta:</p>
+		<form method="post" id="contactus_form" action="#productos-canasta">
+			<?php foreach($clubCanasta->adicionales as $adicional): 
+				$precio = get_post_meta($adicional->ingrediente_id, 'valor_en_puntos', true);
+				$terms = wp_get_post_terms( $adicional->ingrediente_id, 'unidades' );
+				$unidad = isset($terms[0]) ? $terms[0]->name : ''; ?>
+	            
+				<div class="[ margin-bottom ]">
+					<a data-toggle="collapse" href="#ingrediente<?php echo $adicional->ingrediente_id; ?>" class="[ no-decoration color-dark color-dark-hover ]">
+						<button type="submit" class="[ inline-block align-middle ][ btn btn-secondary ]">+</button>
+						<p class="[ inline-block align-middle ][ no-margin ]"><?php echo get_the_title($adicional->ingrediente_id); ?> ( <?php echo $unidad; ?> )</p>
+	                    <input type="hidden" name="padicionalnombre" value="<?php echo get_the_title($adicional->ingrediente_id); ?>">
+					</a>
+					<div id="ingrediente<?php echo $adicional->ingrediente_id; ?>" class="[ panel-collapse collapse ][ padding--top ]">
+						<p class="[ color-gray-xlight ]">Precio: $ <?php echo $precio != '' ? number_format($precio) : 0; ?>
+	                        <input type="hidden" name="padicionalprecio" value="<?php echo $precio != '' ? number_format($precio) : 0; ?>">
+	                    <br>Cantidad: </p>
+	                        <input type="hidden" name="padicionalpeso" value="">
+	                        <input type="hidden" name="padicionaltipo" value="">
+						<div class="[ row ]">
+							<div class="[ col-xs-3 padding--right--small ]">
+								<input type="number" name="padicionalnumber" class="[ width-90 padding--xsmall ][ form-control no-border no-border-radius bg-gray ]">
+							</div>
+							<div class="[ col-xs-5 no-padding ]">
+								<div>
+									<input type="radio" id="option<?php echo $adicional->ingrediente_id; ?>1" name="padicionalocasion" value="Sólo esta ocación">
+									<label for="option<?php echo $adicional->ingrediente_id; ?>1"><span class="[ margin-right--xxsmall ]"></span> Sólo esta ocación</label>
 								</div>
-								<div class="[ col-xs-5 no-padding ]">
-									<div>
-										<input type="radio" id="option11" name="padicionalocasion" value="Sólo esta ocación">
-										<label for="option11"><span class="[ margin-right--xxsmall ]"></span> Sólo esta ocación</label>
-									</div>
-									<div>
-										<input type="radio" id="option12" name="padicionalocasion" value="Cada semana">
-										<label for="option12"><span class="[ margin-right--xxsmall ]"></span> Cada semana</label>
-									</div>
+								<div>
+									<input type="radio" id="option<?php echo $adicional->ingrediente_id; ?>2" name="padicionalocasion" value="Cada semana">
+									<label for="option<?php echo $adicional->ingrediente_id; ?>2"><span class="[ margin-right--xxsmall ]"></span> Cada semana</label>
 								</div>
-								<div class="[ col-xs-4 padding--left--small ]">
-                                    <input type="submit" name="padicional" id="submit" class="[ btn btn-secondary padding--xsmall ][ margin-bottom ]" value="agregar"/>
-								</div>
+							</div>
+							<div class="[ col-xs-4 padding--left--small ]">
+	                            <input type="submit" name="padicional" id="submit" class="[ btn btn-secondary padding--xsmall ][ margin-bottom ]" value="agregar"/>
 							</div>
 						</div>
 					</div>
-                    </form>
-				<?php endforeach;
-			endif;
-		endif; ?>
+				</div>
+	            
+			<?php endforeach; ?>
+		</form>
+	<?php elseif($costoSemanal >= $opCliente->saldo): ?>
+		<p>No cuenta con el saldo sufuciente para agregar adicionales</p>
+	<?php else: ?> 
+		<p>El clube a un no cuenta con productos adicionales</p>
+	<?php endif; ?>
 </article> <!-- /forms -->
